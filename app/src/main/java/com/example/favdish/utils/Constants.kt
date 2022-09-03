31e1daed23ddd37164e0f0e313f623b8b45a1fb5
Teliuk Yaroslav <teluik.yaroslav@gmail.com>
2022-09-03 14:14:27 +0300

    add Glide for images (refactor code create RecycleViewAdaptor, Constant object holder

---
+++ app/src/main/java/com/example/favdish/utils/Constants.kt
@@ -0,0 +1,50 @@
+package com.example.favdish.utils
+
+object Constants {
+
+    const val DISH_TYPE: String = "DishType"
+    const val DISH_CATEGORY: String = "DishCategory"
+    const val DISH_COOKING_TIME: String = "DishCookingTime"
+
+    fun dishTypes(): ArrayList<String>{
+        val list = ArrayList<String>()
+        with(list){
+            add("breakfast")
+            add("lunch")
+            add("snacks")
+            add("dinner")
+            add("salad")
+            add("other")
+        }
+        return list
+    }
+
+    fun dishCategories(): ArrayList<String>{
+        val list = ArrayList<String>()
+        with(list){
+            add("Pizza")
+            add("BBQ")
+            add("Bakery")
+            add("Cafe")
+            add("Chicken")
+            add("Drinks")
+            add("Tea & Coffee")
+            add("Wraps")
+            add("Other")
+        }
+        return list
+    }
+
+    fun dishCookTime(): ArrayList<String>{
+        val list = ArrayList<String>()
+        with(list){
+            add("10")
+            add("15")
+            add("20")
+            add("25")
+            add("30")
+            add("35")
+        }
+        return list
+    }
+}
--- app/src/main/java/com/example/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/favdish/view/activities/AddUpdateDishActivity.kt
@@ -13,22 +13,37 @@ import android.Manifest
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.provider.MediaStore
 import android.provider.Settings
+import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.launch
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.io.*
+import java.util.*
 
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding_root: ActivityAddUpdateDishBinding
+    private var imagePath: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -73,7 +88,20 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
     private val takeImageFromCameraContract = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
         bitmap?.let {
-            binding_root.ivDishImage.setImageBitmap(it)
+            //binding_root.ivDishImage.setImageBitmap(it)
+
+            // draw image in ivDishImage
+            Glide.with(this)
+                .load(it)
+                .centerCrop()
+                .into(binding_root.ivDishImage)
+
+            // save image to internal storage
+            imagePath = saveImageToInternalStorage(it)
+
+            Log.e("ImagePath", imagePath)
+
+            // chancge ivAddDishImage
             binding_root.ivAddDishImage.setImageDrawable(
                 ContextCompat.getDrawable(this,R.drawable.ic_vector_edit))
         }
@@ -83,7 +111,33 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
         if(result.resultCode == Activity.RESULT_OK){
             result?.data?.let {
                 val photoUri = it.data
-                binding_root.ivDishImage.setImageURI(photoUri)
+                //binding_root.ivDishImage.setImageURI(photoUri)
+                Glide.with(this)
+                    .load(photoUri)
+                    .centerCrop()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .listener(object : RequestListener<Drawable>{
+                        override fun onLoadFailed(e: GlideException?, model: Any?,
+                                                  target: Target<Drawable>?,
+                                                  isFirstResource: Boolean ): Boolean {
+                            Log.e("TAG","Error loading image",e)
+                            return false // indicate error
+                        }
+
+                        override fun onResourceReady(resource: Drawable?, model: Any?,
+                                                     target: Target<Drawable>?,
+                                                     dataSource: DataSource?,
+                                                     isFirstResource: Boolean): Boolean {
+                            resource?.let {
+                                val bitmap: Bitmap = resource.toBitmap()
+                                imagePath = saveImageToInternalStorage(bitmap)
+                                Log.e("ImagePath", imagePath)
+                            }
+                            return false
+                        }
+                    })
+                    .into(binding_root.ivDishImage)
+
                 binding_root.ivAddDishImage.setImageDrawable(
                     ContextCompat.getDrawable(this,R.drawable.ic_vector_edit))
             }
@@ -154,6 +208,31 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
             .setNegativeButton("Cancel"){ dialog,_ ->
                 dialog.dismiss()
             }.show()
+    }
+
+    private fun saveImageToInternalStorage(bitmap: Bitmap):String{
+        val wrapper = ContextWrapper(applicationContext)
+
+        // MODE_PRIVATE -> file creation mode
+        var file = wrapper.getDir(IMAGE_DIRECTORY, Context.MODE_PRIVATE)
+        // file = File( folder_to_store, unique_name )
+        file = File(file, "${UUID.randomUUID()}.jpg")
+
+        // compress file
+        try {
+            val stream : OutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+            stream.flush()
+            stream.close()
+        }catch (e: IOException){
+            e.printStackTrace()
+        }
+
+        return file.absolutePath
+    }
+
+    companion object{
+        private val IMAGE_DIRECTORY = "FavDishImages"
     }
 
 }
