d204ab45b04c08bb685e32cf21cd270ec2a143bd
Teliuk Yaroslav <teluik.yaroslav@gmail.com>
2022-09-05 14:11:46 +0300

    add FavDish Adapter

--- app/src/main/java/com/example/favdish/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/favdish/view/fragments/HomeFragment.kt
@@ -4,56 +4,67 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.*
-import android.widget.TextView
+import android.widget.GridLayout
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.favdish.R
 import com.example.favdish.application.FavDishApplication
 import com.example.favdish.databinding.FragmentHomeBinding
 import com.example.favdish.view.activities.AddUpdateDishActivity
+import com.example.favdish.view.adapters.FavDishAdapter
 import com.example.favdish.viewmodel.FavDishViewModel
 import com.example.favdish.viewmodel.FavDishViewModelFactory
 import com.example.favdish.viewmodel.HomeViewModel
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var mBinding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
+    private val binding get() = mBinding!!
 
     private val mFavDishViewModel: FavDishViewModel by viewModels {
         FavDishViewModelFactory( (requireActivity().application as FavDishApplication).repository )
     }
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View
-    {
-        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        mBinding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?)
     {
         super.onViewCreated(view, savedInstanceState)
 
-        // observe any changes and let it data to view
+        binding.rvDishesList.layoutManager = GridLayoutManager(requireActivity(), 2)
+        val favDishAdapter = FavDishAdapter(this@HomeFragment)
+
+        binding.rvDishesList.adapter = favDishAdapter
+
+        // observe any changes and let it data to Log
         mFavDishViewModel.allDishesList.observe(viewLifecycleOwner){ dishes->
             dishes.let {
+                if (it.isNotEmpty()){
+                    binding.rvDishesList.visibility = View.VISIBLE
+                    binding.tvNoDishesAddedYet.visibility = View.GONE
+
+                    favDishAdapter.dishesList(it)
+                }else{
+                    binding.rvDishesList.visibility = View.GONE
+                    binding.tvNoDishesAddedYet.visibility = View.VISIBLE
+                }
+
                 for (item in it){
                     Log.i("Dish Title", "${item.id} :: ${item.title}")
                 }
@@ -88,6 +99,6 @@ class HomeFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+        mBinding = null
     }
 }
